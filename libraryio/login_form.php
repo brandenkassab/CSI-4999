97855be6ffac58bd22faa778584d67f8bec988d4
brandenkassab <61635821+brandenkassab@users.noreply.github.com>
2023-03-04 18:23:49 -0500

    Update login_form.php

--- libraryio/login_form.php
+++ libraryio/login_form.php
@@ -28,7 +28,7 @@ if(isset($_POST['submit'])){
       }elseif($row['user_type'] == 'user'){
 
          $_SESSION['user_name'] = $row['name'];
-         header('location:home.html');
+         header('location:home.php');
 
       }
      
