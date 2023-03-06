c065745283cfcfcfb547ede129ab2f56c664793b
Ashley McGraw-Korthals <101578859+AshleyJM-k@users.noreply.github.com>
2023-03-06 17:16:17 -0500

    Update account_page.php

--- libraryio/account_page.php
+++ libraryio/account_page.php
@@ -9,11 +9,11 @@ $accountId=$_GET['accountId'];
    $row=mysqli_fetch_array($result);
    
  $s='<table align="center" dir="ltr">';   
- $s.='<tr > <td align="left" id="Visitor Name"(change to match DB like "foodName") colspan=2 style="padding:10px; "><h1>'.$row[1].'</h1></td></tr>  
- <td style="vertical-align: text-top; padding-left:20px"><h3>Book Rating: '.$row0[0].'<img src="image/'.$ratingImg.'"/></h3><h3>Current Fines</h3>'.$row[4].
+ $s.='<tr > <td align="left" id="name" colspan=2 style="padding:10px; "><h1>'.$row[1].'</h1></td></tr>  
+ <td style="vertical-align: text-top; padding-left:20px"><h3>Book Rating: '.$row0[0].'<img src="image/'.$ratingImg.'"/></h3><h3>Current Fees</h3>'.$row[4].
  '<br> <h2>Current Books Rented</h2><p>'.$row[3].';
  
-  $q2="SELECT * FROM ((Visitor History)) where recipes_recipe_id((Visitor ID))=".$recipeId;
+  $q2="SELECT * FROM VST_HIST where user_form_id=".$rId;
  $resultf2=mysqli_query($con,$q2)or die(mysqli_error($con));
  while($row2=mysqli_fetch_array($result2)){
   
