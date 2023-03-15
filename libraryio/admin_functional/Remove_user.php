13877669de1f67b7241e1077e18f57ecf23c3e08
Ashley McGraw-Korthals <101578859+AshleyJM-k@users.noreply.github.com>
2023-03-15 07:03:38 -0400

    Update Remove_user.php

--- libraryio/admin_functional/Remove_user.php
+++ libraryio/admin_functional/Remove_user.php
@@ -1,3 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Book</title>
+    
+
+
+    <style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #C2CAD0;
+			padding: 20px;
+		}
+		h1 {
+			color: #333333;
+			margin-bottom: 20px;
+		}
+		form {
+			background-color: #7E685A;
+			padding: 20px;
+			border-radius: 5px;
+			box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+		}
+		label {
+			display: block;
+			margin-bottom: 10px;
+			color: #7E685A;
+			font-weight: bold;
+		}
+		input[type="text"],
+		input[type="number"] {
+			padding: 10px;
+			border: 1px solid #C2B9B0;
+			border-radius: 3px;
+			width: 100%;
+			box-sizing: border-box;
+			margin-bottom: 20px;
+		}
+		input[type="submit"] {
+			background-color: #C2B9B0;
+			color: #ffffff;
+			padding: 10px 20px;
+			border-radius: 3px;
+			border: none;
+			cursor: pointer;
+		}
+		input[type="submit"]:hover {
+			background-color: #C2B9B0;
+		}
+		p.success {
+			color: #C2B9B0;
+			font-weight: bold;
+		}
+		p.error {
+			color: #C2B9B0;
+			font-weight: bold;
+		}
+	
+        .btn {
+			background-color:#C2B9B0 ;
+			color: #7E685A;
+			padding: 10px 20px;
+			border-radius: 3px;
+			border: none;
+			cursor: pointer;
+			margin-top: 20px;
+			display: inline-block;
+			text-decoration: none;
+            
+		}
+		.btn:hover {
+			background-color: #C2B9B0;
+		}
+    
+    
+    </style>
+
 <?php
 
 $host = "localhost";
