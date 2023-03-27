ac18b07c4aa9b3bb48854ea9f9d745e2e2a9d8a0
Ashley McGraw-Korthals <101578859+AshleyJM-k@users.noreply.github.com>
2023-03-27 08:02:23 -0400

    initial commit
    Start of the search bar. Will be a standard search page with nothing fancy to it. Links to the search result page.

---
+++ libraryio/search.php
@@ -0,0 +1,7 @@
+<p> Search</p>
+<form name ="form1" method="post" action="searchresult.php">
+<input name="search" type="text" size="40" maxlength="50"/>
+<input type="submit" name="Submit" value="Search" />
+
+
+</form>
